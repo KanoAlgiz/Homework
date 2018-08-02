5b47310d5487bddce04831e7d0aa4cc467a9cb89
Oboist <englishhorn@mail.ru>
2018-08-02 23:50:51 +0300

    Update Main.java

--- src/localhost/Main.java
+++ src/localhost/Main.java
@@ -18,21 +18,6 @@ public class Main {
 		return xside * yside;
 	}
 
-	private static double square(double x1, double y1 , double x2, double y2){
-		if (y1 > y2){
-			y1 = y1 - y2;
-		} else {
-			y1 = y2 - y1;
-		}
-
-		if (x1 > x2){
-			x1 = x1 - x2;
-		} else {
-			x1 = x2 - x1;
-		}
-
-	return x1 * y1;
-	}
     public static void main(String[] args) {
 		System.out.println(square(0.5, 0.5, -0.5, -0.5));
 }
